0b25cbbb4e56478d2b24bb1f21d6e3ffc9e0193a
mitshubhalf <anonnychat0@gmail.com>
2026-03-27 17:22:58 -0300

    Revise command prompts and remove send functionality
    Updated command usage instructions and removed send command.

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,15 @@
 #include <iostream>
 #include "../include/blockchain.h"
 #include "../include/wallet.h"
-#include "../include/crypto.h"
 
 int main(int argc, char* argv[]) {
     Blockchain blockchain;
 
     if (argc < 2) {
-        std::cout << "Comandos:\n";
-        std::cout << "wallet\n";
-        std::cout << "mine <address>\n";
-        std::cout << "balance <address>\n";
-        std::cout << "send <from> <to> <amount> <privateKey>\n";
+        std::cout << "Uso:\n";
+        std::cout << "./mazechain wallet\n";
+        std::cout << "./mazechain mine <address>\n";
+        std::cout << "./mazechain balance <address>\n";
         return 0;
     }
 
@@ -20,7 +18,6 @@ int main(int argc, char* argv[]) {
     if (cmd == "wallet") {
         Wallet w = createWallet();
         std::cout << "Address: " << w.address << "\n";
-        std::cout << w.privateKey << "\n";
     }
 
     else if (cmd == "mine") {
@@ -29,22 +26,6 @@ int main(int argc, char* argv[]) {
 
     else if (cmd == "balance") {
         std::cout << blockchain.getBalance(argv[2]) << "\n";
-    }
-
-    else if (cmd == "send") {
-        std::string from = argv[2];
-        std::string to = argv[3];
-        double amount = std::stod(argv[4]);
-        std::string priv = argv[5];
-
-        Transaction tx;
-        tx.from = from;
-        tx.to = to;
-        tx.amount = amount;
-
-        tx.signature = signData(tx.toString(), priv);
-
-        blockchain.addTransaction(tx);
     }
 
     return 0;
