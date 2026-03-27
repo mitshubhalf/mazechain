9adf6c869ec6cfe23df566adb69664037c84f645
mitshubhalf <anonnychat0@gmail.com>
2026-03-27 18:00:41 -0300

    Refactor command line usage messages and wallet creation

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,22 @@
-#include <iostream>
 #include "../include/blockchain.h"
-#include "../include/wallet.h"
+#include <iostream>
 
 int main(int argc, char* argv[]) {
-
     Blockchain blockchain;
 
     if (argc < 2) {
-        std::cout << "./mazechain wallet\n";
-        std::cout << "./mazechain mine ADDRESS\n";
-        std::cout << "./mazechain balance ADDRESS\n";
+        std::cout << "Uso:\n";
+        std::cout << "./mazechain mine <address>\n";
+        std::cout << "./mazechain balance <address>\n";
         return 0;
     }
 
     std::string cmd = argv[1];
 
-    if (cmd == "wallet") {
-
-        Wallet w = createWallet();
-
-        std::cout << "Address: " << w.address << "\n";
-        std::cout << "Private: " << w.privateKey << "\n";
-    }
-
-    else if (cmd == "mine") {
-
+    if (cmd == "mine") {
         blockchain.mineBlock(argv[2]);
     }
-
     else if (cmd == "balance") {
-
         std::cout << blockchain.getBalance(argv[2]) << "\n";
     }
 
