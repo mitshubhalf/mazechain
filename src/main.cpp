e87055104926f861d81579a96cfb944f380911cb
mitshubhalf <anonnychat0@gmail.com>
2026-03-27 17:46:46 -0300

    Update command usage instructions in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,28 +3,33 @@
 #include "../include/wallet.h"
 
 int main(int argc, char* argv[]) {
+
     Blockchain blockchain;
 
     if (argc < 2) {
-        std::cout << "Uso:\n";
         std::cout << "./mazechain wallet\n";
-        std::cout << "./mazechain mine <address>\n";
-        std::cout << "./mazechain balance <address>\n";
+        std::cout << "./mazechain mine ADDRESS\n";
+        std::cout << "./mazechain balance ADDRESS\n";
         return 0;
     }
 
     std::string cmd = argv[1];
 
     if (cmd == "wallet") {
+
         Wallet w = createWallet();
+
         std::cout << "Address: " << w.address << "\n";
+        std::cout << "Private: " << w.privateKey << "\n";
     }
 
     else if (cmd == "mine") {
+
         blockchain.mineBlock(argv[2]);
     }
 
     else if (cmd == "balance") {
+
         std::cout << blockchain.getBalance(argv[2]) << "\n";
     }
 
