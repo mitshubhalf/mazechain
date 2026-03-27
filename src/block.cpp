1240f421a1f344ad12f696fd06810e9f934fb86c
mitshubhalf <anonnychat0@gmail.com>
2026-03-27 17:57:28 -0300

    Update block.cpp

--- src/block.cpp
+++ src/block.cpp
@@ -3,17 +3,6 @@
 #include <sstream>
 #include <iomanip>
 
-std::string sha256(std::string input) {
-    unsigned char hash[SHA256_DIGEST_LENGTH];
-    SHA256((unsigned char*)input.c_str(), input.size(), hash);
-
-    std::stringstream ss;
-    for (int i = 0; i < SHA256_DIGEST_LENGTH; i++)
-        ss << std::hex << std::setw(2) << std::setfill('0') << (int)hash[i];
-
-    return ss.str();
-}
-
 Block::Block(int idx, std::vector<Transaction> txs, std::string prev) {
     index = idx;
     transactions = txs;
@@ -26,10 +15,17 @@ std::string Block::calculateHash() {
     std::stringstream ss;
     ss << index << prevHash << nonce;
 
-    for (auto &tx : transactions)
-        ss << tx.toString();
+    std::string data = ss.str();
 
-    return sha256(ss.str());
+    unsigned char hashBytes[SHA256_DIGEST_LENGTH];
+    SHA256((unsigned char*)data.c_str(), data.size(), hashBytes);
+
+    std::stringstream hex;
+    for (int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
+        hex << std::hex << std::setw(2) << std::setfill('0') << (int)hashBytes[i];
+    }
+
+    return hex.str();
 }
 
 void Block::mineBlock(int difficulty) {
