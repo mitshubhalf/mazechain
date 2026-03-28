69a8f5bc02d371e1f5ed60b2a85bc278e27eacef
mitshubhalf <anonnychat0@gmail.com>
2026-03-28 07:50:20 -0300

    Implement random string generation for wallet seed
    Added a random string generator for wallet seeding and modified wallet creation to use it.

--- src/wallet.cpp
+++ src/wallet.cpp
@@ -2,24 +2,41 @@
 #include <openssl/sha.h>
 #include <sstream>
 #include <iomanip>
-#include <cstdlib>
+#include <random>
 
-std::string sha256simple(std::string input) {
+std::string randomString() {
+    std::string chars = "abcdef0123456789";
+    std::string result;
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(0, chars.size() - 1);
+
+    for (int i = 0; i < 32; i++) {
+        result += chars[dis(gen)];
+    }
+
+    return result;
+}
+
+std::string sha256(std::string input) {
     unsigned char hash[SHA256_DIGEST_LENGTH];
     SHA256((unsigned char*)input.c_str(), input.size(), hash);
 
     std::stringstream ss;
-    for(int i=0;i<SHA256_DIGEST_LENGTH;i++)
-        ss<<std::hex<<std::setw(2)<<std::setfill('0')<<(int)hash[i];
+    for (int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
+        ss << std::hex << std::setw(2) << std::setfill('0') << (int)hash[i];
+    }
 
     return ss.str();
 }
 
 Wallet createWallet() {
+    std::string seed = randomString();
+    std::string address = "MC-" + sha256(seed).substr(0, 20);
 
-    std::string priv = std::to_string(rand()) + std::to_string(rand());
+    Wallet w;
+    w.address = address;
 
-    std::string address = "MC-" + sha256simple(priv).substr(0,20);
-
-    return {address, priv};
+    return w;
 }
