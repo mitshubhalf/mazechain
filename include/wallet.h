4025d1e5812d4e0710cb7f19b4890c60d0eabdcf
mitshubhalf <anonnychat0@gmail.com>
2026-03-28 08:04:32 -0300

    Update wallet.h

--- include/wallet.h
+++ include/wallet.h
@@ -1,8 +1,13 @@
-#pragma once
+#ifndef WALLET_H
+#define WALLET_H
+
 #include <string>
 
 struct Wallet {
     std::string address;
+    std::string privateKey;
 };
 
 Wallet createWallet();
+
+#endif
