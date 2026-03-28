36e05d0fdecb684430f128348b5b8386796ec6b2
mitshubhalf <anonnychat0@gmail.com>
2026-03-28 08:56:10 -0300

    Add SHA-256 hashing function to wallet.cpp

--- src/wallet.cpp
+++ src/wallet.cpp
@@ -1,6 +1,18 @@
 #include "../include/wallet.h"
-#include "../include/crypto.h"
-#include <cstdlib>
+#include <openssl/sha.h>
+#include <sstream>
+#include <iomanip>
+
+std::string sha256simple(const std::string &input) {
+    unsigned char hash[SHA256_DIGEST_LENGTH];
+    SHA256((unsigned char*)input.c_str(), input.size(), hash);
+
+    std::stringstream ss;
+    for(int i = 0; i < SHA256_DIGEST_LENGTH; i++)
+        ss << std::hex << std::setw(2) << std::setfill('0') << (int)hash[i];
+
+    return ss.str();
+}
 
 Wallet createWallet() {
     std::string priv = std::to_string(rand()) + std::to_string(rand());
