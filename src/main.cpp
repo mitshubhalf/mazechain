009e5d6d57224ea62b19f441b6e31d01eb902186
mitshubhalf <anonnychat0@gmail.com>
2026-03-29 10:59:49 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,49 @@
-#include "../include/blockchain.h"
-#include "../include/wallet.h"
 #include <iostream>
+#include "wallet.h"
+#include "blockchain.h"
 
 int main(int argc, char* argv[]) {
 
-    Blockchain blockchain;
-
-    if (argc < 2) {
-        std::cout << "Uso:\n";
-        std::cout << "./mazechain wallet\n";
-        std::cout << "./mazechain mine <address>\n";
-        std::cout << "./mazechain balance <address>\n";
+    if(argc < 2) {
+        std::cout << "./mazechain createwallet\n";
+        std::cout << "./mazechain mine\n";
+        std::cout << "./mazechain balance\n";
+        std::cout << "./mazechain validate\n";
         return 0;
     }
 
     std::string cmd = argv[1];
 
-    if (cmd == "wallet") {
-        Wallet w = createWallet();
+    if(cmd == "createwallet") {
+        Wallet w;
+        w.generateMnemonic();
+        w.generateFromMnemonic();
+        w.saveToFile("wallet.dat");
 
+        std::cout << "Wallet criada\n";
+        std::cout << "Seed: " << w.mnemonic << "\n";
         std::cout << "Address: " << w.address << "\n";
-        std::cout << "Private Key: " << w.privateKey << "\n";
     }
-    else if (cmd == "mine") {
-        if (argc < 3) {
-            std::cout << "Informe o endereço\n";
-            return 0;
-        }
-        blockchain.mineBlock(argv[2]);
+
+    if(cmd == "mine") {
+        Wallet w;
+        w.loadFromFile("wallet.dat");
+
+        Blockchain bc;
+        bc.mineBlock(w.address);
     }
-    else if (cmd == "balance") {
-        if (argc < 3) {
-            std::cout << "Informe o endereço\n";
-            return 0;
-        }
-        std::cout << blockchain.getBalance(argv[2]) << "\n";
+
+    if(cmd == "balance") {
+        Wallet w;
+        w.loadFromFile("wallet.dat");
+
+        Blockchain bc;
+        std::cout << "Balance: " << bc.getBalance(w.address) << " MC\n";
+    }
+
+    if(cmd == "validate") {
+        Blockchain bc;
+        std::cout << (bc.isValid() ? "Chain válida\n" : "Chain inválida\n");
     }
 
     return 0;
