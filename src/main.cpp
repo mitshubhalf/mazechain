94b7f6df903514c8715e866b12345d2b0141d846
mitshubhalf <anonnychat0@gmail.com>
2026-03-29 12:45:52 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,52 +3,59 @@
 #include "blockchain.h"
 #include "transaction.h"
 
-// precisa do crypto.cpp
 std::string signData(const std::string&, const std::string&);
 
 int main(int argc,char* argv[]){
 
- if(argc<2){
-  std::cout<<"createwallet | mine | balance | send\n";
-  return 0;
- }
+    if(argc<2){
+        std::cout<<"createwallet | mine | balance | send\n";
+        return 0;
+    }
 
- std::string cmd=argv[1];
+    std::string cmd=argv[1];
 
- if(cmd=="createwallet"){
-  Wallet w;
-  w.generateMnemonic();
-  w.generateFromMnemonic();
-  w.saveToFile("wallet.dat");
+    if(cmd=="createwallet"){
+        Wallet w;
+        w.generateMnemonic();
+        w.generateFromMnemonic();
+        w.saveToFile("wallet.dat");
 
-  std::cout<<"Address: "<<w.address<<"\n";
- }
+        std::cout<<"Address: "<<w.address<<"\n";
+    }
 
- if(cmd=="mine"){
-  Wallet w; w.loadFromFile("wallet.dat");
-  Blockchain bc;
-  bc.mineBlock(w.address);
- }
+    if(cmd=="mine"){
+        Wallet w; w.loadFromFile("wallet.dat");
+        Blockchain bc;
+        bc.mineBlock(w.address);
+    }
 
- if(cmd=="balance"){
-  Wallet w; w.loadFromFile("wallet.dat");
-  Blockchain bc;
-  std::cout<<bc.getBalance(w.address)<<"\n";
- }
+    if(cmd=="balance"){
+        Wallet w; w.loadFromFile("wallet.dat");
+        Blockchain bc;
+        std::cout<<"Balance: "<<bc.getBalance(w.address)<<" MC\n";
+    }
 
- if(cmd=="send"){
-  Wallet w; w.loadFromFile("wallet.dat");
+    if(cmd=="send"){
 
-  Transaction tx;
-  tx.from=w.address;
-  tx.to=argv[2];
-  tx.amount=stoi(argv[3]);
+        if(argc < 4){
+            std::cout<<"Uso: send ADDRESS AMOUNT\n";
+            return 0;
+        }
 
-  tx.signature=signData(tx.toString(),w.privateKey);
+        Wallet w; 
+        w.loadFromFile("wallet.dat");
 
-  Blockchain bc;
-  bc.addTransaction(tx);
- }
+        Transaction tx;
+        tx.from = w.address;
+        tx.to = argv[2];
+        tx.amount = std::stoi(argv[3]);
 
- return 0;
+        tx.publicKey = w.publicKey; // 🔥 ESSENCIAL
+        tx.signature = signData(tx.toString(), w.privateKey);
+
+        Blockchain bc;
+        bc.addTransaction(tx);
+    }
+
+    return 0;
 }
