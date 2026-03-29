0898006d963cd0282f810d822fcbd06806a36a86
mitshubhalf <anonnychat0@gmail.com>
2026-03-29 10:56:46 -0300

    Enhance Wallet class with mnemonic and file operations
    Added functions to generate mnemonic, save and load wallet data.

--- src/wallet.cpp
+++ src/wallet.cpp
@@ -1,22 +1,53 @@
-#include "../include/wallet.h"
+#include "wallet.h"
 #include <openssl/sha.h>
 #include <sstream>
 #include <iomanip>
+#include <fstream>
+#include <vector>
+#include <cstdlib>
 
-std::string sha256simple(const std::string &input) {
+std::vector<std::string> wordlist = {
+    "apple","banana","cat","dog","eagle","forest","gold","house",
+    "ice","jungle","king","lemon","moon","night","ocean","power",
+    "queen","river","sun","tree","unity","victory","world","zebra"
+};
+
+Wallet::Wallet() {}
+
+std::string Wallet::sha256(const std::string& input) {
     unsigned char hash[SHA256_DIGEST_LENGTH];
     SHA256((unsigned char*)input.c_str(), input.size(), hash);
 
     std::stringstream ss;
-    for(int i = 0; i < SHA256_DIGEST_LENGTH; i++)
+    for(int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
         ss << std::hex << std::setw(2) << std::setfill('0') << (int)hash[i];
-
+    }
     return ss.str();
 }
 
-Wallet createWallet() {
-    std::string priv = std::to_string(rand()) + std::to_string(rand());
-    std::string address = "MC-" + sha256simple(priv).substr(0, 20);
+void Wallet::generateMnemonic() {
+    mnemonic = "";
+    for(int i = 0; i < 12; i++) {
+        mnemonic += wordlist[rand() % wordlist.size()];
+        if(i < 11) mnemonic += " ";
+    }
+}
 
-    return {address, priv};
+void Wallet::generateFromMnemonic() {
+    privateKey = sha256(mnemonic);
+    publicKey = sha256(privateKey);
+    address = sha256(publicKey).substr(0, 40);
+}
+
+void Wallet::saveToFile(const std::string& filename) {
+    std::ofstream file(filename);
+    file << mnemonic << "\n" << privateKey << "\n" << publicKey << "\n" << address;
+}
+
+void Wallet::loadFromFile(const std::string& filename) {
+    std::ifstream file(filename);
+    std::getline(file, mnemonic);
+    std::getline(file, privateKey);
+    std::getline(file, publicKey);
+    std::getline(file, address);
 }
