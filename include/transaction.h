c26fd8b470e5671ddc5ed890088968648c559c40
mitshubhalf <anonnychat0@gmail.com>
2026-03-29 11:36:57 -0300

    Update transaction.h

--- include/transaction.h
+++ include/transaction.h
@@ -1,13 +1,16 @@
-#pragma once
+#ifndef TRANSACTION_H
+#define TRANSACTION_H
 #include <string>
 
 struct Transaction {
-    std::string from;
-    std::string to;
-    double amount;
-    std::string signature;
+ std::string from;
+ std::string to;
+ int amount;
+ std::string signature;
 
-    std::string toString() const {
-        return from + to + std::to_string(amount);
-    }
+ std::string toString() const {
+  return from+to+std::to_string(amount);
+ }
 };
+
+#endif
