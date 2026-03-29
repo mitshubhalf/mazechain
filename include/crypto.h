34fbec1c0c781e241166494fcf511036c6fb56ff
mitshubhalf <anonnychat0@gmail.com>
2026-03-29 16:31:57 -0300

    Update crypto.h

--- include/crypto.h
+++ include/crypto.h
@@ -1,10 +1,8 @@
-#pragma once
+#ifndef CRYPTO_H
+#define CRYPTO_H
+
 #include <string>
 
-std::string sha256(const std::string &data);
+std::string sha256(const std::string &input);
 
-std::string generatePrivateKey();
-std::string getPublicKey(const std::string &privateKey);
-
-std::string signData(const std::string &data, const std::string &privateKey);
-bool verifySignature(const std::string &data, const std::string &signature, const std::string &publicKey);
+#endif
