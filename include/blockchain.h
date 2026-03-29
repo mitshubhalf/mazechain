91698056936729357dabb2363aa0ec70de34ea77
mitshubhalf <anonnychat0@gmail.com>
2026-03-29 10:57:16 -0300

    Refactor blockchain.h with include guards and access modifiers

--- include/blockchain.h
+++ include/blockchain.h
@@ -1,24 +1,32 @@
-#pragma once
-#include "block.h"
+#ifndef BLOCKCHAIN_H
+#define BLOCKCHAIN_H
+
 #include <vector>
 #include <string>
 
+struct Block {
+    int index;
+    std::string data;
+    std::string prevHash;
+    std::string hash;
+};
+
 class Blockchain {
-private:
-    std::vector<Block> chain;
-    std::vector<Transaction> mempool;
-
-    int difficulty;
-    double totalSupply;
-
 public:
+    std::vector<Block> chain;
+
     Blockchain();
 
-    void mineBlock(std::string miner);
-    double getBalance(std::string address);
+    void mineBlock(const std::string& miner);
+    int getBalance(const std::string& address);
 
-    int getBlockReward(int height);
+    void save();
+    void load();
 
-    void saveChain();
-    void loadChain();
+    bool isValid();
+
+private:
+    std::string sha256(const std::string& input);
 };
+
+#endif
