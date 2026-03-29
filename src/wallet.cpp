00d514f2813c15e981daf2fcaf4dbe9a3a09254e
mitshubhalf <anonnychat0@gmail.com>
2026-03-29 11:36:24 -0300

    Update wallet.cpp

--- src/wallet.cpp
+++ src/wallet.cpp
@@ -7,47 +7,46 @@
 #include <cstdlib>
 
 std::vector<std::string> wordlist = {
-    "apple","banana","cat","dog","eagle","forest","gold","house",
-    "ice","jungle","king","lemon","moon","night","ocean","power",
-    "queen","river","sun","tree","unity","victory","world","zebra"
+ "apple","banana","cat","dog","eagle","forest","gold","house",
+ "ice","jungle","king","lemon","moon","night","ocean","power",
+ "queen","river","sun","tree","unity","victory","world","zebra"
 };
 
 Wallet::Wallet() {}
 
 std::string Wallet::sha256(const std::string& input) {
-    unsigned char hash[SHA256_DIGEST_LENGTH];
-    SHA256((unsigned char*)input.c_str(), input.size(), hash);
+ unsigned char hash[SHA256_DIGEST_LENGTH];
+ SHA256((unsigned char*)input.c_str(), input.size(), hash);
 
-    std::stringstream ss;
-    for(int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
-        ss << std::hex << std::setw(2) << std::setfill('0') << (int)hash[i];
-    }
-    return ss.str();
+ std::stringstream ss;
+ for(int i=0;i<SHA256_DIGEST_LENGTH;i++)
+  ss<<std::hex<<std::setw(2)<<std::setfill('0')<<(int)hash[i];
+
+ return ss.str();
 }
 
 void Wallet::generateMnemonic() {
-    mnemonic = "";
-    for(int i = 0; i < 12; i++) {
-        mnemonic += wordlist[rand() % wordlist.size()];
-        if(i < 11) mnemonic += " ";
-    }
+ for(int i=0;i<12;i++){
+  mnemonic+=wordlist[rand()%wordlist.size()];
+  if(i<11) mnemonic+=" ";
+ }
 }
 
 void Wallet::generateFromMnemonic() {
-    privateKey = sha256(mnemonic);
-    publicKey = sha256(privateKey);
-    address = sha256(publicKey).substr(0, 40);
+ privateKey=sha256(mnemonic);
+ publicKey=sha256(privateKey);
+ address=sha256(publicKey).substr(0,40);
 }
 
-void Wallet::saveToFile(const std::string& filename) {
-    std::ofstream file(filename);
-    file << mnemonic << "\n" << privateKey << "\n" << publicKey << "\n" << address;
+void Wallet::saveToFile(const std::string& filename){
+ std::ofstream f(filename);
+ f<<mnemonic<<"\n"<<privateKey<<"\n"<<publicKey<<"\n"<<address;
 }
 
-void Wallet::loadFromFile(const std::string& filename) {
-    std::ifstream file(filename);
-    std::getline(file, mnemonic);
-    std::getline(file, privateKey);
-    std::getline(file, publicKey);
-    std::getline(file, address);
+void Wallet::loadFromFile(const std::string& filename){
+ std::ifstream f(filename);
+ getline(f,mnemonic);
+ getline(f,privateKey);
+ getline(f,publicKey);
+ getline(f,address);
 }
