10d198fa94c01638e3c5ad2681eddcb45264e846
mitshubhalf <anonnychat0@gmail.com>
2026-03-29 16:33:23 -0300

    Update block.cpp

--- src/block.cpp
+++ src/block.cpp
@@ -1,31 +1,27 @@
 #include "../include/block.h"
-#include <openssl/sha.h>
-#include <sstream>
-#include <iomanip>
+#include "../include/crypto.h"
+#include <ctime>
 
-Block::Block(int idx, std::vector<Transaction> txs, std::string prev) {
+Block::Block(int idx, std::vector<std::string> txs, std::string prevHash) {
     index = idx;
     transactions = txs;
-    prevHash = prev;
+    previousHash = prevHash;
     nonce = 0;
+
+    time_t now = time(0);
+    timestamp = ctime(&now);
+
     hash = calculateHash();
 }
 
 std::string Block::calculateHash() {
-    std::stringstream ss;
-    ss << index << prevHash << nonce;
+    std::string data = std::to_string(index) + timestamp + previousHash + std::to_string(nonce);
 
-    std::string data = ss.str();
-
-    unsigned char hashBytes[SHA256_DIGEST_LENGTH];
-    SHA256((unsigned char*)data.c_str(), data.size(), hashBytes);
-
-    std::stringstream hex;
-    for (int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
-        hex << std::hex << std::setw(2) << std::setfill('0') << (int)hashBytes[i];
+    for (auto &tx : transactions) {
+        data += tx;
     }
 
-    return hex.str();
+    return sha256(data);
 }
 
 void Block::mineBlock(int difficulty) {
