35c91988c0c638eda858cc405f29ec51ec9f9692
mitshubhalf <anonnychat0@gmail.com>
2026-03-29 23:28:24 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,18 @@
 int main() {
     Blockchain mazechain;
 
-    std::cout << "⛏️ Minerando bloco 1...\n";
-    mazechain.addBlock(Block(1, {"Tx1: A -> B (10 MC)"}, mazechain.getLatestBlock().hash));
+    mazechain.addBlock(Block(1, {"A -> B: 10 MC"}, mazechain.getLatestBlock().hash));
+    mazechain.addBlock(Block(2, {"B -> C: 5 MC"}, mazechain.getLatestBlock().hash));
 
-    std::cout << "⛏️ Minerando bloco 2...\n";
-    mazechain.addBlock(Block(2, {"Tx2: B -> C (5 MC)"}, mazechain.getLatestBlock().hash));
+    std::cout << "\n=== MAZECHAIN ===\n";
 
     for (auto &block : mazechain.chain) {
-        std::cout << "\n====================\n";
+        std::cout << "\n-------------------------\n";
         std::cout << "Index: " << block.index << "\n";
+        std::cout << "Timestamp: " << block.timestamp;
         std::cout << "Hash: " << block.hash << "\n";
-        std::cout << "Prev: " << block.previousHash << "\n";
+        std::cout << "Prev Hash: " << block.previousHash << "\n";
+        std::cout << "Nonce: " << block.nonce << "\n";
     }
 
     return 0;
