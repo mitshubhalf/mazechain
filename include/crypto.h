21cc704dd86db20c29e23de35f95ba77617de60f
mitshubhalf <anonnychat0@gmail.com>
2026-03-31 14:31:55 -0300

    Update crypto.h

--- include/crypto.h
+++ include/crypto.h
@@ -3,6 +3,13 @@
 
 #include <string>
 
-std::string sha256(const std::string &input);
+namespace Crypto {
+
+    std::string sha256(const std::string &input);
+
+    // 🔁 Usado em blockchains como Bitcoin
+    std::string doubleSHA256(const std::string &input);
+
+}
 
 #endif
