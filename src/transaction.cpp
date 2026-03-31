80df80740f3220c50b653620fb6c106cc6672e96
mitshubhalf <anonnychat0@gmail.com>
2026-03-31 14:27:20 -0300

    Update transaction.cpp

--- src/transaction.cpp
+++ src/transaction.cpp
@@ -1,1 +1,20 @@
+#include "../include/transaction.h"
+#include <sstream>
 
+Transaction::Transaction() {
+    from = "";
+    to = "";
+    amount = 0.0;
+}
+
+Transaction::Transaction(std::string from, std::string to, double amount) {
+    this->from = from;
+    this->to = to;
+    this->amount = amount;
+}
+
+std::string Transaction::toString() const {
+    std::stringstream ss;
+    ss << from << "->" << to << ":" << amount;
+    return ss.str();
+}
