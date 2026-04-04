b6d3103b61cbce58ea3c7393884e4a8457e1a9cf
mitshubhalf <anonnychat0@gmail.com>
2026-04-04 10:43:04 -0300

    Update blockchain.h

--- include/blockchain.h
+++ include/blockchain.h
@@ -1,26 +1,33 @@
 #ifndef BLOCKCHAIN_H
 #define BLOCKCHAIN_H
 
-#include "block.h"
 #include <vector>
+#include "block.h"
+#include "transaction.h"
 
 class Blockchain {
 private:
     std::vector<Block> chain;
+    int difficulty;
 
 public:
-    int difficulty;
+    std::vector<Transaction> pendingTransactions;
 
     Blockchain();
 
-    Block createGenesisBlock();
-    Block getLatestBlock();
-
     void addBlock(Block newBlock);
+    Block getLatestBlock() const;
+
+    void minePendingTransactions(std::string minerAddress);
+
+    int getBalance(const std::string& address) const;
+    bool addTransaction(const Transaction& tx);
+
+    void printChain() const;
 
     const std::vector<Block>& getChain() const;
     void clearChain();
-    void addLoadedBlock(const Block& block);
+    void addLoadedBlock(const Block& b);
 };
 
 #endif
