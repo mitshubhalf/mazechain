68099b12d90850b05a12407aa9ec41c26e09c9b6
mitshubhalf <anonnychat0@gmail.com>
2026-04-04 15:45:25 -0300

    Update transaction.cpp

--- src/transaction.cpp
+++ src/transaction.cpp
@@ -1,31 +1,21 @@
 #include "../include/transaction.h"
+#include <sstream>
 
+// construtor vazio
 Transaction::Transaction() {
     from = "";
     to = "";
     amount = 0;
 }
 
-Transaction::Transaction(std::string from, std::string to, int amount) {
-    this->from = from;
-    this->to = to;
-    this->amount = amount;
-}
-
-std::string Transaction::toString() const {
-    return from + "->" + to + ":" + std::to_string(amount);
-}
-#include "../include/transaction.h"
-#include <sstream>
-
-Transaction::Transaction() {}
-
+// construtor principal
 Transaction::Transaction(std::string from, std::string to, double amount) {
     this->from = from;
     this->to = to;
     this->amount = amount;
 }
 
+// converter para string
 std::string Transaction::toString() const {
     std::stringstream ss;
     ss << from << "->" << to << ":" << amount;
