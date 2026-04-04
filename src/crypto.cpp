376adc67988e969c1c7eabdf45eed9f0c2e8e6bf
mitshubhalf <anonnychat0@gmail.com>
2026-04-04 09:53:08 -0300

    Update crypto.cpp

--- src/crypto.cpp
+++ src/crypto.cpp
@@ -3,17 +3,17 @@
 #include <sstream>
 #include <iomanip>
 
+namespace Crypto {
+
 std::string sha256(const std::string &input) {
     unsigned char hash[SHA256_DIGEST_LENGTH];
-
-    SHA256(reinterpret_cast<const unsigned char*>(input.c_str()), input.size(), hash);
+    SHA256((unsigned char*)input.c_str(), input.size(), hash);
 
     std::stringstream ss;
-    ss << std::hex << std::setfill('0');
-
-    for (int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
-        ss << std::setw(2) << static_cast<unsigned int>(hash[i]);
+    for(int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
+        ss << std::hex << std::setw(2) << std::setfill('0') << (int)hash[i];
     }
-
     return ss.str();
+}
+
 }
