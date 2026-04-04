cf7dde44005877a79e753d02739fe9a2d0bd8b95
mitshubhalf <anonnychat0@gmail.com>
2026-04-04 15:24:57 -0300

    Update blockchain.cpp

--- src/blockchain.cpp
+++ src/blockchain.cpp
@@ -4,54 +4,41 @@ Blockchain::Blockchain() {
     chain.push_back(Block(0, {}, "0"));
 }
 
+void Blockchain::addBlock(Block newBlock) {
+    chain.push_back(newBlock);
+}
+
 Block Blockchain::getLatestBlock() const {
     return chain.back();
 }
 
-void Blockchain::minePendingTransactions(std::string minerAddress) {
-
-    // recompensa
-    pendingTransactions.push_back(Transaction("SYSTEM", minerAddress, 50));
-
-    Block newBlock(chain.size(), pendingTransactions, getLatestBlock().hash);
-
-    newBlock.mineBlock(4);
-
-    chain.push_back(newBlock);
-
-    pendingTransactions.clear();
-}
-
-void Blockchain::addTransaction(const Transaction& tx) {
-    pendingTransactions.push_back(tx);
-}
-
-int Blockchain::getBalance(std::string address) {
-
-    int balance = 0;
-
-    for (const auto& block : chain) {
-        for (const auto& tx : block.transactions) {
-
-            if (tx.from == address)
-                balance -= tx.amount;
-
-            if (tx.to == address)
-                balance += tx.amount;
-        }
-    }
-
-    return balance;
-}
-
-const std::vector<Block>& Blockchain::getChain() const {
-    return chain;
+void Blockchain::clearChain() {
+    chain.clear();
 }
 
 void Blockchain::addLoadedBlock(const Block& block) {
     chain.push_back(block);
 }
 
-void Blockchain::clearChain() {
-    chain.clear();
+std::vector<Block>& Blockchain::getChain() {
+    return chain;
+}
+
+void Blockchain::addTransaction(const Transaction& tx) {
+    if (!chain.empty()) {
+        chain.back().transactions.push_back(tx);
+    }
+}
+
+double Blockchain::getBalance(const std::string& address) {
+    double balance = 0;
+
+    for (const auto& block : chain) {
+        for (const auto& tx : block.transactions) {
+            if (tx.from == address) balance -= tx.amount;
+            if (tx.to == address) balance += tx.amount;
+        }
+    }
+
+    return balance;
 }
