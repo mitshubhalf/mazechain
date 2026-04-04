39ccf97ea7651683d0f1526c0d45dd164f257d52
mitshubhalf <anonnychat0@gmail.com>
2026-04-04 10:40:44 -0300

    Update block.h

--- include/block.h
+++ include/block.h
@@ -3,19 +3,20 @@
 
 #include <string>
 #include <vector>
+#include "transaction.h"
 
 class Block {
 public:
     int index;
     std::string timestamp;
-    std::vector<std::string> transactions;
+    std::vector<Transaction> transactions;
     std::string previousHash;
     std::string hash;
     int nonce;
 
-    Block(int idx, std::vector<std::string> txs, std::string prevHash);
+    Block(int idx, std::vector<Transaction> txs, std::string prevHash);
 
-    std::string calculateHash();
+    std::string calculateHash() const;
     void mineBlock(int difficulty);
 };
 
