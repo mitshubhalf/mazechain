61eb6801872c79be378378cd99fd6ace455b490c
mitshubhalf <anonnychat0@gmail.com>
2026-04-04 14:42:46 -0300

    Update blockchain.cpp

--- src/blockchain.cpp
+++ src/blockchain.cpp
@@ -1,37 +1,57 @@
 #include "../include/blockchain.h"
-#include <iostream>
 
 Blockchain::Blockchain() {
-    difficulty = 3;
+    chain.push_back(Block(0, {}, "0"));
 }
 
-Block Blockchain::createGenesisBlock() {
-    return Block(0, {"Genesis Block"}, "0");
-}
-
-Block Blockchain::getLatestBlock() {
+Block Blockchain::getLatestBlock() const {
     return chain.back();
 }
 
-void Blockchain::addBlock(Block newBlock) {
-    newBlock.previousHash = getLatestBlock().hash;
+void Blockchain::minePendingTransactions(std::string minerAddress) {
 
-    std::cout << "⛏️ Mining block " << newBlock.index << "...\n";
-    newBlock.mineBlock(difficulty);
+    // recompensa do minerador
+    pendingTransactions.push_back(Transaction("SYSTEM", minerAddress, 50));
+
+    Block newBlock(chain.size(), pendingTransactions, getLatestBlock().hash);
+
+    newBlock.mineBlock(4);
 
     chain.push_back(newBlock);
 
-    std::cout << "✅ Block mined: " << newBlock.hash << "\n";
+    pendingTransactions.clear();
+}
+
+void Blockchain::addTransaction(const Transaction& tx) {
+    pendingTransactions.push_back(tx);
+}
+
+int Blockchain::getBalance(std::string address) {
+
+    int balance = 0;
+
+    for (const auto& block : chain) {
+        for (const auto& tx : block.transactions) {
+
+            if (tx.from == address)
+                balance -= tx.amount;
+
+            if (tx.to == address)
+                balance += tx.amount;
+        }
+    }
+
+    return balance;
 }
 
 const std::vector<Block>& Blockchain::getChain() const {
     return chain;
 }
 
-void Blockchain::clearChain() {
-    chain.clear();
-}
-
 void Blockchain::addLoadedBlock(const Block& block) {
     chain.push_back(block);
+}
+
+void Blockchain::clearChain() {
+    chain.clear();
 }
