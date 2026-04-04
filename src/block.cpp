2e411dacb763df3aebe8fa87603fc4883137e1d4
mitshubhalf <anonnychat0@gmail.com>
2026-04-04 09:40:31 -0300

    Update block.cpp

--- src/block.cpp
+++ src/block.cpp
@@ -10,16 +10,17 @@ Block::Block(int idx, std::vector<std::string> txs, std::string prevHash) {
     nonce = 0;
 
     time_t now = time(0);
-    timestamp = std::to_string(now);
+    timestamp = std::ctime(&now);
+    timestamp.pop_back(); // 🔥 REMOVE \n (CRÍTICO)
 
     hash = calculateHash();
 }
 
-std::string Block::calculateHash() const {
+std::string Block::calculateHash() {
     std::stringstream ss;
     ss << index << timestamp << previousHash << nonce;
 
-    for (const auto &tx : transactions) {
+    for (auto &tx : transactions) {
         ss << tx;
     }
 
@@ -29,13 +30,8 @@ std::string Block::calculateHash() const {
 void Block::mineBlock(int difficulty) {
     std::string target(difficulty, '0');
 
-    while (hash.compare(0, difficulty, target) != 0) {
+    while (hash.substr(0, difficulty) != target) {
         nonce++;
         hash = calculateHash();
-
-        // opcional: log de progresso
-        // if (nonce % 100000 == 0) {
-        //     std::cout << "Nonce: " << nonce << "\r";
-        // }
     }
 }
