cb21f5218ea60f5098de70519c524504c441e0bc
mitshubhalf <anonnychat0@gmail.com>
2026-04-08 15:35:28 -0300

    Update block.h

--- include/block.h
+++ include/block.h
@@ -1,17 +1,16 @@
 #ifndef BLOCK_H
 #define BLOCK_H
 
-#include <string>
 #include <vector>
+#include <string>
 #include "transaction.h"
 
 class Block {
 public:
     int index;
-    std::string timestamp;
-    std::vector<Transaction> transactions;
     std::string previousHash;
     std::string hash;
+    std::vector<Transaction> transactions;
     int nonce;
 
     Block(int idx, std::vector<Transaction> txs, std::string prevHash);
