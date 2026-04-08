5eaad734aab65a85c7ee27b41484358d96800895
mitshubhalf <anonnychat0@gmail.com>
2026-04-08 16:16:31 -0300

    Update blockchain.h

--- include/blockchain.h
+++ include/blockchain.h
@@ -2,38 +2,33 @@
 #define BLOCKCHAIN_H
 
 #include <vector>
-#include <string>
 #include "block.h"
-#include "transaction.h"
+
+struct UTXO {
+    std::string txId;
+    int index;
+    std::string address;
+    double amount;
+};
 
 class Blockchain {
 private:
     std::vector<Block> chain;
     std::vector<Transaction> pendingTransactions;
+    std::vector<UTXO> utxoPool;
 
 public:
     Blockchain();
 
-    void addBlock(Block newBlock);
     Block getLatestBlock() const;
-
-    void clearChain();
-    void addLoadedBlock(const Block& block);
-
-    std::vector<Block>& getChain();
-    const std::vector<Block>& getChain() const;
+    void addBlock(Block block);
 
     void addTransaction(const Transaction& tx);
     void minePendingTransactions(const std::string& minerAddress);
 
     double getBalance(const std::string& address) const;
 
-    // 🔒 validação
-    bool isChainValid() const;
-
-    // 🔥 PREPARADO PRO PRÓXIMO PASSO (mempool persistente)
-    const std::vector<Transaction>& getPendingTransactions() const;
-    void setPendingTransactions(const std::vector<Transaction>& txs);
+    const std::vector<Block>& getChain() const;
 };
 
 #endif
