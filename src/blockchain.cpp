63e47ef3ba3e4c4416e64f81999ac97a98afdad1
mitshubhalf <anonnychat0@gmail.com>
2026-04-08 12:50:06 -0300

    Update blockchain.cpp

--- src/blockchain.cpp
+++ src/blockchain.cpp
@@ -1,5 +1,4 @@
 #include "../include/blockchain.h"
-#include "../include/blockchain.h"
 
 // CONSTRUTOR
 Blockchain::Blockchain() {
@@ -45,7 +44,6 @@ void Blockchain::addTransaction(const Transaction& tx) {
 // ⛏️ MINERAR
 void Blockchain::minePendingTransactions(const std::string& minerAddress) {
 
-    // reward
     Transaction reward("", minerAddress, 50);
     pendingTransactions.push_back(reward);
 
@@ -77,7 +75,7 @@ double Blockchain::getBalance(const std::string& address) const {
     return balance;
 }
 
-// 🔒 VALIDAÇÃO DA CHAIN
+// 🔒 VALIDAÇÃO
 bool Blockchain::isChainValid() const {
 
     for (size_t i = 1; i < chain.size(); i++) {
@@ -93,70 +91,4 @@ bool Blockchain::isChainValid() const {
     }
 
     return true;
-}
-// CONSTRUTOR
-Blockchain::Blockchain() {
-    chain.push_back(Block(0, {}, "0"));
-}
-
-// ÚLTIMO BLOCO
-Block Blockchain::getLatestBlock() const {
-    return chain.back();
-}
-
-// GET CHAIN
-std::vector<Block>& Blockchain::getChain() {
-    return chain;
-}
-
-const std::vector<Block>& Blockchain::getChain() const {
-    return chain;
-}
-
-// LIMPAR
-void Blockchain::clearChain() {
-    chain.clear();
-}
-
-// ADD BLOCO CARREGADO
-void Blockchain::addLoadedBlock(const Block& block) {
-    chain.push_back(block);
-}
-
-// ADD TRANSAÇÃO
-void Blockchain::addTransaction(const Transaction& tx) {
-    pendingTransactions.push_back(tx);
-}
-
-// ⛏️ MINERAR
-void Blockchain::minePendingTransactions(const std::string& minerAddress) {
-
-    Transaction reward("", minerAddress, 50);
-    pendingTransactions.push_back(reward);
-
-    Block newBlock(chain.size(), pendingTransactions, getLatestBlock().hash);
-
-    newBlock.mineBlock(4);
-
-    chain.push_back(newBlock);
-
-    pendingTransactions.clear();
-}
-
-// 💰 SALDO
-double Blockchain::getBalance(const std::string& address) const {
-    double balance = 0;
-
-    for (const auto& block : chain) {
-        for (const auto& tx : block.transactions) {
-
-            if (!tx.from.empty() && tx.from == address)
-                balance -= tx.amount;
-
-            if (!tx.to.empty() && tx.to == address)
-                balance += tx.amount;
-        }
-    }
-
-    return balance;
 }
