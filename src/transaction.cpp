193294f238c154cf9bdfddcf3882f3aa92d404d1
mitshubhalf <anonnychat0@gmail.com>
2026-04-08 15:59:08 -0300

    Update transaction.cpp

--- src/transaction.cpp
+++ src/transaction.cpp
@@ -1,23 +1,7 @@
 #include "../include/transaction.h"
-#include <sstream>
 
 Transaction::Transaction() {
     id = "";
-}
-
-std::string Transaction::toString() const {
-    std::stringstream ss;
-
-    ss << "Transaction ID: " << id << "\n";
-
-    for (const auto& in : inputs) {
-        ss << "  Input: " << in.txId << ":" << in.outputIndex
-           << " (" << in.address << ")\n";
-    }
-
-    for (const auto& out : outputs) {
-        ss << "  Output: " << out.address << " -> " << out.amount << "\n";
-    }
-
-    return ss.str();
+    inputs.clear();
+    outputs.clear();
 }
