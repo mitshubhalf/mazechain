4af51b2c5302d7ae31341405fc8e2455017b5a6b
mitshubhalf <anonnychat0@gmail.com>
2026-04-08 15:34:32 -0300

    Update transaction.cpp

--- src/transaction.cpp
+++ src/transaction.cpp
@@ -1,9 +1,23 @@
 #include "../include/transaction.h"
 #include <sstream>
 
-// converter para string
+Transaction::Transaction() {
+    id = "";
+}
+
 std::string Transaction::toString() const {
     std::stringstream ss;
-    ss << from << "->" << to << ":" << amount;
+
+    ss << "Transaction ID: " << id << "\n";
+
+    for (const auto& in : inputs) {
+        ss << "  Input: " << in.txId << ":" << in.outputIndex
+           << " (" << in.address << ")\n";
+    }
+
+    for (const auto& out : outputs) {
+        ss << "  Output: " << out.address << " -> " << out.amount << "\n";
+    }
+
     return ss.str();
 }
