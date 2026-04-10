af033345b5b3064c3313c4950c19f23e7eaf62c1
mitshubhalf <anonnychat0@gmail.com>
2026-04-10 12:48:18 -0300

    Update blockchain.h

--- include/blockchain.h
+++ include/blockchain.h
@@ -1,35 +1,33 @@
+// include/blockchain.h
 #ifndef BLOCKCHAIN_H
 #define BLOCKCHAIN_H
 
-#include <vector>
 #include <string>
-#include "block.h"
-#include "transaction.h"
+#include <vector>
 
-class Blockchain {
-private:
-    std::vector<Block> chain;
-    int difficulty;
-    double totalSupply;
-
-    const int DIFFICULTY_ADJUSTMENT_INTERVAL = 10; 
-    const int TARGET_BLOCK_TIME = 30;             
-
-public:
-    Blockchain();
-    Block getLastBlock();
-    double getBlockReward(int height);
-    void mineBlock(std::string minerAddress);
-    double getBalance(std::string address);
-    void send(std::string from, std::string to, double amount);
-    
-    // Gerenciamento e Persistência
-    void adjustDifficulty();
-    std::vector<Block> getChain() const;
-    void addBlock(const Block& block);
-    void clearChain();
-    int getDifficulty() const;
-    void setDifficulty(int d); 
+struct VOut {
+    std::string address;
+    double amount;
 };
 
+struct Transaction {
+    std::vector<std::string> vin; // Entradas (simplificadas)
+    std::vector<VOut> vout;       // Saídas
+};
+
+class Block {
+public:
+    int index;
+    long timestamp;
+    std::string hash;
+    std::string prevHash; // Certifique-se que aqui é prevHash
+    long nonce;
+    std::vector<Transaction> transactions;
+
+    Block(int idx, std::string prev, std::vector<Transaction> txs);
+    std::string calculateHash() const;
+    void mine(int difficulty);
+};
+
+// ... restante da classe Blockchain
 #endif
