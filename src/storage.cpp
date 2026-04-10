592d09af0d3a6b49e9876d38320f5343eadad14f
mitshubhalf <anonnychat0@gmail.com>
2026-04-10 11:26:01 -0300

    Update storage.cpp

--- src/storage.cpp
+++ src/storage.cpp
@@ -14,13 +14,9 @@ void Storage::saveChain(const Blockchain& bc, const std::string& filename) {
 
         for (const auto& tx : block.transactions) {
             file << "TX\n" << tx.vin.size() << "\n";
-            for (const auto& in : tx.vin) {
-                file << in.txid << "\n" << in.index << "\n";
-            }
+            for (const auto& in : tx.vin) file << in.txid << "\n" << in.index << "\n";
             file << tx.vout.size() << "\n";
-            for (const auto& out : tx.vout) {
-                file << out.address << "\n" << out.amount << "\n";
-            }
+            for (const auto& out : tx.vout) file << out.address << "\n" << out.amount << "\n";
         }
         file << "END_BLOCK\n";
     }
@@ -36,40 +32,29 @@ void Storage::loadChain(Blockchain& bc, const std::string& filename) {
 
     while (file >> line) {
         if (line == "DIFFICULTY") {
-            int d;
-            file >> d;
-            bc.setDifficulty(d);
+            int d; file >> d; bc.setDifficulty(d);
         } else if (line == "BLOCK") {
-            int tIdx, tNonce;
-            long tTime;
-            std::string tPrev, tHash;
-            std::vector<Transaction> tTxs;
-
-            file >> tIdx;
-            file.ignore(1000, '\n');
+            int tIdx, tNonce; long tTime; std::string tPrev, tHash;
+            file >> tIdx; file.ignore(1000, '\n');
             std::getline(file >> std::ws, tPrev);
             std::getline(file >> std::ws, tHash);
             file >> tNonce >> tTime;
 
+            std::vector<Transaction> tTxs;
             std::string subLine;
             while (file >> subLine && subLine != "END_BLOCK") {
                 if (subLine == "TX") {
-                    int vinS, voutS;
-                    file >> vinS;
+                    int vinS, voutS; file >> vinS;
                     std::vector<TxIn> vins;
                     for (int i = 0; i < vinS; i++) {
-                        TxIn in;
-                        file >> std::ws; std::getline(file, in.txid);
-                        file >> in.index;
-                        vins.push_back(in);
+                        TxIn in; file >> std::ws; std::getline(file, in.txid);
+                        file >> in.index; vins.push_back(in);
                     }
                     file >> voutS;
                     std::vector<TxOut> vouts;
                     for (int i = 0; i < voutS; i++) {
-                        TxOut out;
-                        file >> std::ws; std::getline(file, out.address);
-                        file >> out.amount;
-                        vouts.push_back(out);
+                        TxOut out; file >> std::ws; std::getline(file, out.address);
+                        file >> out.amount; vouts.push_back(out);
                     }
                     tTxs.push_back(Transaction(vins, vouts));
                 }
