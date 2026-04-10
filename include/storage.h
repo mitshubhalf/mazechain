271fabcf35152b3e85864a468f94eb6a13b7002b
mitshubhalf <anonnychat0@gmail.com>
2026-04-10 12:51:57 -0300

    Update storage.h

--- include/storage.h
+++ include/storage.h
@@ -1,16 +1,19 @@
 #ifndef STORAGE_H
 #define STORAGE_H
 
-#include "blockchain.h"
 #include <vector>
 #include <string>
+
+// Forward Declaration: Avisa ao compilador que essa classe existe
+// sem precisar dar o include do blockchain.h aqui.
+class Blockchain;
+struct Transaction;
 
 class Storage {
 public:
     static void saveChain(const Blockchain& bc, const std::string& filename);
     static void loadChain(Blockchain& bc, const std::string& filename);
     
-    // Novas funções para Mempool
     static void saveMempool(const Transaction& tx, const std::string& filename);
     static std::vector<Transaction> loadMempool(const std::string& filename);
     static void clearMempool(const std::string& filename);
