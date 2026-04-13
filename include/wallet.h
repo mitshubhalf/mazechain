b4521cb057048d687ca1431578bc68e64dfe1e3d
mitshubhalf <anonnychat0@gmail.com>
2026-04-13 15:20:05 -0300

    Update wallet.h

--- include/wallet.h
+++ include/wallet.h
@@ -8,8 +8,10 @@ public:
     std::string address;
     std::string seed;
 
+    // Gera uma seed aleatória e um endereço
     void create(); 
-    // Novo método para evitar o erro de compilação
+    
+    // Recupera o endereço a partir de uma seed existente
     void fromSeed(const std::string& existingSeed); 
 };
 
