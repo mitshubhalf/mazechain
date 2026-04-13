c0dc6b2f4fdd6bfddd338d3aaae59374df03cd31
mitshubhalf <anonnychat0@gmail.com>
2026-04-13 10:38:10 -0300

    Update transaction.cpp

--- src/transaction.cpp
+++ src/transaction.cpp
@@ -1,29 +1,20 @@
 #include "../include/transaction.h"
+#include "../include/crypto.h"
 #include <sstream>
-#include <openssl/sha.h>
 
-Transaction::Transaction(std::vector<TxIn> in, std::vector<TxOut> out)
-    : vin(in), vout(out) {
-    id = calculateHash();
+// Construtor opcional para inicialização rápida
+Transaction::Transaction() {
+    id = "";
+    signature = "";
+    publicKey = "";
 }
 
+// Lógica para gerar o hash único da transação
 std::string Transaction::calculateHash() const {
     std::stringstream ss;
-
-    for (auto &i : vin)
-        ss << i.txid << i.index;
-
-    for (auto &o : vout)
-        ss << o.address << o.amount;
-
-    std::string data = ss.str();
-
-    unsigned char hash[SHA256_DIGEST_LENGTH];
-    SHA256((unsigned char*)data.c_str(), data.size(), hash);
-
-    std::stringstream result;
-    for (int i = 0; i < SHA256_DIGEST_LENGTH; i++)
-        result << std::hex << (int)hash[i];
-
-    return result.str();
+    for (const auto& out : vout) {
+        ss << out.address << out.amount;
+    }
+    // Adiciona timestamp ou algo único se necessário
+    return Crypto::sha256_util(ss.str());
 }
