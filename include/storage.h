0d60e2dc2fa02701eccbbcb94d8cb76a8393155f
mitshubhalf <anonnychat0@gmail.com>
2026-04-14 17:42:05 -0300

    Update storage.h

--- include/storage.h
+++ include/storage.h
@@ -5,14 +5,19 @@
 #include <vector>
 #include "transaction.h"
 
+// Forward declaration da classe Blockchain
+// Isso evita que o Storage tente incluir o Blockchain que tenta incluir o Storage (loop)
 class Blockchain; 
 
 class Storage {
 public:
+    // Persistência da Corrente de Blocos
     static void saveChain(const Blockchain& bc, const std::string& filename);
-    // Alterado para bool para o main.cpp funcionar
+    
+    // Retorna true se o arquivo existir e for carregado com sucesso
     static bool loadChain(Blockchain& bc, const std::string& filename);
     
+    // Gestão da Mempool (Transações aguardando mineração)
     static void saveMempool(const Transaction& tx, const std::string& filename);
     static std::vector<Transaction> loadMempool(const std::string& filename);
     static void clearMempool(const std::string& filename);
