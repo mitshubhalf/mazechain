8d45d623692170bb8788e8bb37ce0b670a2e4b9b
mitshubhalf <anonnychat0@gmail.com>
2026-04-14 17:40:44 -0300

    Update crypto.h

--- include/crypto.h
+++ include/crypto.h
@@ -3,10 +3,16 @@
 
 #include <string>
 #include <vector>
-#include "transaction.h"
+
+// Forward declaration para evitar inclusão circular 
+// (Já que Transaction usa Crypto e Crypto usa Transaction)
+struct Transaction; 
 
 namespace Crypto {
+    // Retorna o hash SHA256 de qualquer string em formato hexadecimal
     std::string sha256_util(std::string str);
+
+    // Constrói a árvore de Merkle e retorna a raiz (ID de integridade do bloco)
     std::string calculateMerkleRoot(const std::vector<Transaction>& txs);
 }
 
