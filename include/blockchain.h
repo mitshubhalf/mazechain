d53d73f848d0e4d28929714d2a923ca43f936cc0
mitshubhalf <anonnychat0@gmail.com>
2026-04-18 16:51:45 -0300

    Update blockchain.h

--- include/blockchain.h
+++ include/blockchain.h
@@ -29,16 +29,12 @@ public:
     Block getLastBlock() const { return chain.back(); }
     void clearChain();
     
-    // --- NOVA LÓGICA ECONÔMICA (INCENTIVO 2000 MZ) ---
-    // Regras Atualizadas: 
-    // 0-1000 blocos: 2000 MZ
-    // 1001-2000 blocos: 250 MZ
-    // 2001-4000 blocos: 125 MZ
     double getBlockReward(int height);
-    
     double getBalance(std::string address);
-    double getTotalSupply() const { return totalSupply; }
-    double getMaxSupply() const { return MAX_SUPPLY; }
+    
+    // APENAS AS DECLARAÇÕES AQUI (A lógica fica no .cpp)
+    double getTotalSupply() const;
+    double getMaxSupply() const;
     
     void send(std::string from, std::string to, double amount, std::string seed);
     bool verifyTransaction(const Transaction& tx);
